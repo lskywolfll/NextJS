5e696223ac66a5cd18c6a739aa95e43fe16f26ae
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-04-25 23:46:42 -0400

    se agrega una ruta automatizada por nextjs que desde una carpeta dentro de pages sera tomada como un path

---
+++ pages/testing/index.js
@@ -0,0 +1,11 @@
+export class index extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Desde el otro lugar</h1>
+            </div>
+        )
+    }
+}
+
+export default index
