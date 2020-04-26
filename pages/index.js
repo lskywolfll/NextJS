0a147fb0dfaf315a9d2bf49cce22c3f80c3fd121
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-04-25 23:40:01 -0400

    se deja la primera pagina!

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// React por defecto como es un modulo de dependencia podemos ejecutarlo sin necesidad de importarlo
+export default class extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hola!</h1>
+                <p>Bienvenidos</p>
+            </div>
+        )
+    }
+}
