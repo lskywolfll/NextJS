38f8654f6b0a55a3d011e1fbceb472c1ee4ceda0
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-04-26 01:02:14 -0400

    se actualiza el uso de los archivos staticos para nextjs y se cumple el reto de la pagina extra cosa aplicada ya antes jeje

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,46 @@ export default class extends React.Component {
             <div>
                 <h1>Hola!</h1>
                 <p>Bienvenidos</p>
+
+                <img src="/static/platzi-logo.png" alt="platzi" />
+
+                {/* Esto solo aplica para componente y no se aplicara a componentes internos o externos */}
+                <style jsx>{`
+                h1 {
+                    color: red;
+                }
+
+                p {
+                    color: green;
+                }
+                
+                // Esto se usa para saltar el scope normal y hacer el estilo global
+                :global(p){
+                    color:green;
+                }
+
+                img {
+                    max-width: 50%;
+                    display: block;
+                    margin: 0 auto;
+                }
+                // 
+                // component :global(propierty css)
+                // {
+                //    property: value;
+                // }
+                // 
+            `}</style>
+            {/* Esta es una manera de romper la regla normal y hacerla global
+                pero esto solo debe usarse cuando realmente sea necesario 
+
+                Lo primero es ver con la primera regla normal del jsx style
+            */}
+            <style jsx global>{`
+                body {
+                    background: white;
+                }
+            `}</style>
             </div>
         )
     }
--- pages/testing/index.js
+++ pages/testing/index.js
@@ -2,8 +2,42 @@ export class index extends React.Component {
     render() {
         return (
             <div>
+
+                <img src="/static/platzi-logo.png" alt="platzi" />
+
                 <h1>Desde el otro lugar</h1>
+                <p>Pagina testing</p>
+
+                <style jsx>
+                    {`
+                        div {
+                            text-align: center;
+                        }
+
+                        h1 {
+                            color: white;
+                        }
+
+                        p {
+                            color: white;
+                        }
+
+                        img {
+                            margin-top: 10%;
+                            max-width: 10%;
+                            display: block;
+                            margin: 10% auto auto auto;
+                        }
+
+                        :global(body){
+                            background-color: #192b34;
+                        }
+                    `}
+                </style>
             </div>
+
+            // Estilos unicos en este componente y en ningun otro lugar mas
+
         )
     }
 }
